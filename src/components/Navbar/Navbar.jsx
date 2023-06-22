c4a91ce66a5a689d2c1bba1d195ec84962dde446
ahmadillo20007 <ahmadilloqubijonov88@gmail.com>
2023-06-22 14:53:29 +0500

    e

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,52 +1,45 @@
 import React from 'react'
-import './Navbar.css'
-import logo from '../../img/logo.png'
-import news from '../../img/new.png'
-import med from '../../img/med.png'
-import shop from '../../img/shop.png'
-import team from '../../img/team.png'
-import statis from '../../img/statis.png'
-import aka from '../../img/aka.png'
-import home from '../../img/home.png'
-function Navbar() {
+import './NavBar.css'
+import shirt1 from '../../assets/Img/futbolka1.png'
+import cap1 from '../../assets/Img/kepka1.png'
+import tap1 from '../../assets/Img/tapchika.png'
+import bag1 from '../../assets/Img/sumka.png'
+
+function NavBar() {
   return (
-   <div className="navbar">
-    <div className="container">
-      <img className='navbar__logo' src={logo} alt="" />
-      <ul className='navbar__list'>
-        <hr />
-        <li className='navbar__list__item'>
-        Bosh sahifa <img className='list__item' src={home} alt="" />
-        </li>
-        <hr />
-        <li className='navbar__list__item'>
-        Yangiliklar <img className='list__item' src={news} alt="" />
-        </li>
-        <hr />
-        <li className='navbar__list__item'>
-        Media <img className='list__item' src={med} alt="" />
-        </li>
-        <hr />
-        <li className='navbar__list__item'>
-        Do’kon <img className='list__item' src={shop} alt="" />
-        </li>
-        <hr />
-        <li className='navbar__list__item'>
-        Klub <img className='list__item' src={team} alt="" />
-        </li>
-        <hr />
-        <li className='navbar__list__item'>
-        Statistika <img className='list__item' src={statis} alt="" />
-        </li>
-        <hr />
-        <li className='navbar__list__item'>
-        Akademiya <img className='list__item' src={aka} alt="" />
-        </li>
-        <hr />
-      </ul>
+    <div className='Navbar'> 
+        <div className="container">
+            <div className='Left__right'>
+                <div className="Left">
+                    <div className='Left__card'>
+                        <img className='Card__img1' src={shirt1} alt="" />
+                        <h3 className='Card__title1'>New Shirt from ROMA FC</h3>
+                        <button className='Card__button1'>Buy Now <i className="bi bi-arrow-right  Card__arrow"></i></button>
+                    </div>
+                </div>
+                <div className="Right">
+                <div className="Right__card1">
+                        <img className='Right__img1' src={cap1} alt="" />
+                        <h3 className='Right__card__title1'>New Cap from ROMA FC</h3>
+                        <button className='Right__btn1'>Buy Now <i className="bi bi-arrow-right  Card__arrow"></i></button>
+                </div>
+                <div className="Card__right__left">
+                    <div className="Card__left">
+                        <img className='Card__left__img1' src={tap1} alt="" />
+                        <h3 className='Card__left__title1'>New accessories</h3>
+                        <button className='Card__left__btn1'>Buy Now <i className="bi bi-arrow-right  Card__arrow"></i></button>
+                    </div>
+                    <div className="Card__right">
+                    <img className='Card__right__img1' src={bag1} alt="" />
+                        <h3 className='Card__right__title1'>New Bag</h3>
+                        <button className='Card__right__btn1'>Buy Now <i className="bi bi-arrow-right  Card__arrow"></i></button>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
     </div>
-   </div>
   )
 }
 
-export default Navbar
+export default NavBar
